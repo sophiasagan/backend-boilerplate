6cf1af4b8ac8fa84bd24711401ddb0e6b75deba1
sophiasagan <sophiabraddock82@gmail.com>
2020-05-17 15:43:19 -0400

    basics added

---
+++ index.js
@@ -0,0 +1,38 @@
+const express = require("express")
+const helmet = require("helmet")
+const cookieParser = require("cookie-parser")
+const cors = require("cors")
+const authRouter = require("./auth/auth-router")
+const usersRouter = require("./users/users-router")
+
+const server = express()
+const port = process.env.PORT || 5000
+
+server.use(helmet())
+server.use(cookieParser())
+server.use(express.json())
+
+server.use(cors({
+	credentials: true,
+	origin: "http://localhost:3000",
+}))
+
+server.use("/auth", authRouter)
+server.use("/users", usersRouter)
+
+server.get("/", (req, res, next) => {
+	res.json({
+		message: "Welcome to our API",
+	})
+})
+
+server.use((err, req, res, next) => {
+	console.log(err)
+	res.status(500).json({
+		message: "Something went wrong",
+	})
+})
+
+server.listen(port, () => {
+	console.log(`Running at http://localhost:${port}`)
+})
